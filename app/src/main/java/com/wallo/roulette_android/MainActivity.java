514bd3ee70de623b7a464c263798ddf6e8625800
02wallo <02skidrow@naver.com>
2016-11-17 20:22:45 +0900

    Butter Knife 라이브러리 추가.

--- app/src/main/java/com/wallo/roulette_android/MainActivity.java
+++ app/src/main/java/com/wallo/roulette_android/MainActivity.java
@@ -4,12 +4,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
+    @OnClick (R.id.btn_appang) void appang(){
+        Log.i("reward", "appang");
+    }
+    @OnClick (R.id.btn_tnk) void tnk(){
+        Log.i("reward", "tnk");
+    }
+    @OnClick (R.id.btn_adpopcorn) void adpopcorn(){
+        Log.i("reward", "adpopcorn");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("test", "test");
+        ButterKnife.bind(this);
+
     }
 }
